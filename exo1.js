be01c5fcc90131a81a65f66fa52c567cc0f30402
Sanogo-Aicha <aichatasanogo381@gmail.com>
2024-08-31 11:17:46 +0000

    first commit

---
+++ exo1.js
@@ -0,0 +1,81 @@
+/*exo1:
+
+var ch="sister fatim je taime"
+var vol="aieou"
+var conte=0
+    for (let index = 0; index < ch.length; index++) {
+    for (let i = 0; i < vol.length; i++) {
+        if(ch[index]==vol[i])
+           conte+=1
+          
+    }
+    
+}
+alert(conte)
+
+function voell(p){//exo2
+     if (p%2===0){
+        console.log('Even');
+        
+     }
+     else console.log('old');
+     
+}
+console.log(voell(23))
+
+
+function nombre(nomb){//exo4
+    if(nomb > 0){
+        return -nomb
+    }
+    else if(nomb< 0){
+        return nomb
+    }
+}
+console.log(nombre(34))
+
+
+function nombe(nomb1,p){ exo6:
+    
+    if(nomb1==p*p){ exo6
+        return true
+    }
+    else if(nomb1 !==p*p){
+        return false
+    }
+}
+alert(nombe(23,3))
+
+EXO9:
+
+var ch="xxxXoooOO";
+var conte=0
+var compte=0
+for (let index = 0; index < ch.length; index++) {
+    if(ch[index]=="o" || ch[index]=="O"){
+        conte+=1
+    }
+    
+}
+
+for (let i = 0; i < ch.length; i++) {
+    if(ch[i]=="x" || ch[i]=="X"){
+        compte+=1
+    }
+    
+}
+alert(conte)
+alert(compte)
+if(compte==conte){
+    alert(true)
+}
+else if(compte!==conte) {
+    alert(false)
+}
+    EXO10:
+    let array=1+"-"+9+"-"+9+"-"+9+"-"+3;
+for (let index = 0; index < array.length; index++) {
+    alert(array[index]*array[index])
+    
+}
+*/
